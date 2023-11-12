c79eedfcd78cb29c063c06624cc997844c9804f1
mainbosskay <dadakehinde80@yahoo.com>
2023-11-12 11:44:01 -0800

    Main.h file

--- main.h
+++ main.h
@@ -1,48 +1,69 @@
 #ifndef header
 #define header
-#include <stdlib.h>
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+
 /**
- * struct fmtflags - This structure contain flags
+ * struct fmtflags - Structure containing flags
  * @space: ' ' character
  * @hash: '#' character
  * @plus: '+' character
  */
+
 typedef struct fmtflags
 {
 	int space;
 	int hash;
 	int plus;
-} fmtflags_t;
+}fmtflags_t;
 
 /**
  * struct fmtprint - Mapping format specifiers
  * @c: format specifier
- * @s: print function
+ * @a: print function
  */
+
 typedef struct fmtprint
 {
 	char c;
-	int (*s)(va_list arg, fmtflags_t *s);
+	int (*s)(va_list ag, fmtflags_t *s);
 } fmtprint_h;
-int integer_print (va_list k, fmtflags_t *s);
+
+/* num_print.c */
+int integer_print(va_list k, fmtflags_t *s);
 void num_print(int a);
 int unsigned_int_print(va_list k, fmtflags_t *s);
 int num_count(int c);
+
+/* num_conversion.c */
 int print_hex_lower(va_list k, fmtflags_t *s);
 int print_hex_upper(va_list k, fmtflags_t *s);
+int print_base_two(va_list k, fmtflags_t *s);
+int print_base_eight(va_list k, fmtflags_t *s);
 char *_sprintf(unsigned long int dgt, int base, int lowercase);
+
+/* _printf.c */
 int _printf(const char *format, ...);
-int (*handle_print(char b)) (va_list, fmtflags_t *s);
-int_handle_flag(char b, fmtflags_t *s);
+
+/* format_handling.c */
+int (*handle_print(char b))(va_list, fmtflags_t *s);
+int handle_flag(char b, fmtflags_t *s);
+
+/* char_print.c */
 int str_print(va_list k, fmtflags_t *s);
 int char_print(va_list k, fmtflags_t *s);
 int _putchar(char c);
 int _puts(char *str);
 int rot13_print(va_list k, fmtflags_t *s);
+
+/* char_manip.c */
 int reverse_str(va_list k, fmtflags_t *s);
 int nonprint_char(va_list k, fmtflags_t *s);
 int pointer_print(va_list k, fmtflags_t *s);
 int percent_print(va_list k, fmtflags_t *s);
+
 #endif
+
