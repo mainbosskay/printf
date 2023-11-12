954053a0e7f28e29a47736a782174274dbe2a678
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-12 08:44:56 -0800

    _printf

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _printf - Printing formatted output
+ * @format: The first parameter
+ * @...: The list for more arguments
+ * Return: total characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, fmtflags_t *);
+	const char *b;
+	va_list args;
+	fmtflags_t fmtflags = (0, 0, 0);
+
+	int sum = 0;
+
+	va_start(args, format);
+	if (!format || (format[0] == '%' !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (b = format; *b; b++)
+	{
+		if (*b == '%')
+		{
+			b++;
+			if (*b == '%')
+			{
+				sum += _putchar('%');
+				continue;
+			}
+			while (handle_flag(*b, &fmtflags))
+				b++;
+			pfunc = handle_print(*b);
+			sum += (pfunc);
+			? pfunc(args, &fmtflags)
+				: _printf("%%%c", *b);
+		}
+		else
+			sum += _putchar(*b);
+	}
+	_putchar(-1);
+	va_end(args);
+	return (sum);
+}
